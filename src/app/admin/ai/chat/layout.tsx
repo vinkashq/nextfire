4890003a5f482d6255caf3dc5782a1e432429ba0
Vinoth Kannan <svkn.87@gmail.com>
2026-02-16 12:06:23 +0530

    refactor: update Resizable components to use `orientation` prop and percentage `defaultSize` values.

--- src/app/admin/ai/chat/layout.tsx
+++ src/app/admin/ai/chat/layout.tsx
@@ -3,12 +3,12 @@ import ChatList from "./list";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <ResizablePanelGroup direction="horizontal">
-      <ResizablePanel defaultSize={25}>
+    <ResizablePanelGroup orientation="horizontal">
+      <ResizablePanel defaultSize="25%">
         <ChatList />
       </ResizablePanel>
       <ResizableHandle />
-      <ResizablePanel defaultSize={75} className="px-4">{children}</ResizablePanel>
+      <ResizablePanel defaultSize="75%" className="px-4">{children}</ResizablePanel>
     </ResizablePanelGroup>
   )
 }
